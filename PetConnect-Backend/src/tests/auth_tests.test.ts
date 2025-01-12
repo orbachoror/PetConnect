7dafe0132ae4befe893e22603ba6198018470308
Yonatan Isakov <yonatan156354@gmail.com>
2025-01-12 06:35:07 +0200

    tests added

--- PetConnect-Backend/src/tests/auth_tests.test.ts
+++ PetConnect-Backend/src/tests/auth_tests.test.ts
@@ -1,16 +1,24 @@
 import request from 'supertest';
 import app from '../app';
 import connectToDB from '../db';
-import User, { IUser } from '../models/user_model';
+import User from '../models/user_model';
 import mongoose from 'mongoose';
 import logger from '../utils/logger';
 
-const testUser: IUser = {
+interface UserInfo {
+    name: string,
+    email: string,
+    password: string,
+    accessToken?: string,
+    refreshToken?: string,
+    _id?: string
+}
+const testUser: UserInfo = {
     name: 'testName',
     email: 'testMail',
-    password: 'testPassword'
+    password: 'testPassword',
 }
-const invalidUser: IUser = {
+const invalidUser: UserInfo = {
     name: 'testName',
     email: 'fakeMail',
     password: ''
@@ -48,6 +56,15 @@ test('Login with exist user', async () => {
         .post('/auth/login')
         .send(testUser);
     expect(response.status).toBe(200);
+    const accessToken = response.body.accessToken;
+    const refreshToken = response.body.refreshToken;
+    const _id = response.body._id;
+    expect(accessToken).toBeDefined();
+    expect(refreshToken).toBeDefined();
+    expect(_id).toBeDefined();
+    testUser.accessToken = accessToken;
+    testUser.refreshToken = refreshToken;
+    testUser._id = _id;
 });
 test('Login with not existent user', async () => {
     const response = await request(app)
@@ -55,3 +72,35 @@ test('Login with not existent user', async () => {
         .send(invalidUser);
     expect(response.status).not.toBe(200);
 });
+test('Logout', async () => {
+    const response = await request(app)
+        .post('/auth/logout')
+        .send({ refreshToken: testUser.refreshToken });
+    expect(response.status).toBe(200);
+});
+test('Refresh token multiple times valid and invalid', async () => {
+    const response = await request(app)
+        .post('/auth/login')
+        .send(testUser);
+    expect(response.status).toBe(200);
+    const refreshToken = response.body.refreshToken;
+    const response2 = await request(app)
+        .post('/auth/refresh')
+        .send({ refreshToken: refreshToken });
+    expect(response2.status).toBe(200);
+    const refreshToken2 = response2.body.refreshToken;
+    expect(refreshToken2).toBeDefined();
+    expect(refreshToken).not.toBe(refreshToken2);
+    const response3 = await request(app)
+        .post('/auth/refresh')
+        .send({ refreshToken: refreshToken });
+    expect(response3.status).not.toBe(200);
+    const response4 = await request(app)
+        .post('/auth/refresh')
+        .send({ refreshToken: refreshToken2 });
+    expect(response4.status).not.toBe(200);
+
+});
+
+
+

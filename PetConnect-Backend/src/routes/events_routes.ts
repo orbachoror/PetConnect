d5a8c3cbb5b8fa186802ea867421b70bee3138a8
orbachoror <orbachoror@cs.colman.ac.il>
2025-01-18 23:10:14 +0200

    Implement Swagger

--- PetConnect-Backend/src/routes/events_routes.ts
+++ PetConnect-Backend/src/routes/events_routes.ts
@@ -12,3 +12,161 @@ router.put('/:id', authMiddleware, ownershipMiddleware(Event), eventsController.
 router.delete('/:id', authMiddleware, ownershipMiddleware(Event), eventsController.deleteItem.bind(eventsController));
 
 export default router;
+
+
+/**
+* @swagger
+* tags:
+*   name: Events
+*   description: The Events API
+*/
+// ************************************** Event Schema *******************************************
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Event:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - date
+ *         - location
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The event title
+ *         description:
+ *           type: string
+ *           description: The event description
+ *         date:
+ *           type: string
+ *           format: date
+ *           description: The event date
+ *         location:
+ *           type: string
+ *           description: The event location
+ *         owner:
+ *           type: string
+ *           description: The user ID
+ */
+// ************************************** Get all events *******************************************
+/**
+ * @swagger
+ * /events:
+ *   get:
+ *     summary: Get all events
+ *     tags: [Events]
+ *     responses:
+ *       200:
+ *         description: List of all events
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Event'
+ */
+// ************************************** Get event by ID *******************************************
+/**
+ * @swagger
+ * /events/{id}:
+ *   get:
+ *     summary: Get an event by ID
+ *     tags: [Events]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the event to fetch
+ *         schema:
+ *           type: string
+ *           example: "60d0fe4f5311236168a109cd"
+ *     responses:
+ *       200:
+ *         description: The event
+ */
+// ************************************** Create event *******************************************
+/**
+ * @swagger
+ * /events:
+ *   post:
+ *     summary: Create an event
+ *     tags: [Events]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Event'
+ *           example:
+ *             title: "Event Title"
+ *             description: "Event Description"
+ *             date: "2021-06-22T00:00:00.000Z"
+ *             location: "Event Location"
+ *     responses:
+ *       200:
+ *         description: The event
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Event'
+ */
+
+// ************************************** Update event *******************************************
+/**
+ * @swagger
+ * /events/{id}:
+ *   put:
+ *     summary: Update an event
+ *     tags: [Events]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the event to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Event'
+ *           example:
+ *             title: "Event Title updated"
+ *             description: "Event Description updated"
+ *             date: "2021-06-22T00:00:00.000Z"
+ *             location: "Event Location updated"
+ *     responses:
+ *       200:
+ *         description: The event
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Event'
+ */
+// ************************************** Delete event *******************************************
+/**
+ * @swagger
+ * /events/{id}:
+ *   delete:
+ *     summary: Delete an event
+ *     tags: [Events]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the event to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The event was deleted
+ */
--- PetConnect-Backend/src/routes/users_routes.ts
+++ PetConnect-Backend/src/routes/users_routes.ts
@@ -1,3 +1,4 @@
+
 import Router from 'express';
 import usersController from '../controllers/users_controller';
 import { authMiddleware } from '../middleware/auth_middleware';
@@ -7,3 +8,95 @@ router.get("/", authMiddleware, usersController.getById.bind(usersController));
 router.put('/', authMiddleware, usersController.updateItem.bind(usersController));
 
 export default router;
+
+/**
+* @swagger
+* tags:
+*   name: User
+*   description: The User API
+*/
+//* ************************************** User Schema *******************************************
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The user's name
+ *         email:
+ *           type: string
+ *           description: The user's email
+ *         password:
+ *           type: string
+ *           description: The user's password
+ *         phone:
+ *           type: string
+ *           description: The user's phone number
+ *         address:
+ *           type: string
+ *           description: The user's address
+ *         dateOfBirth:
+ *           type: string
+ *           description: The user's date of birth
+ *         refreshTokens:
+ *           type: string
+ *           description: The user's refresh tokens
+ *         _id:
+ *           type: string
+ *           description: The user's ID
+ */
+
+//* ************************************** Get user by ID *******************************************
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: The user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user was not found
+ */
+
+//* ************************************** Update user *******************************************
+/**
+ * @swagger
+ * /user:
+ *   put:
+ *     summary: Update a user
+ *     tags: [User]
+ *     security:
+ *      - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user was not found
+ *       500:
+ *         description: Internal server error
+ */
